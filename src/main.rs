18210b450cc9f43f8120bcf7676a5ce01990bbdf
Blake Ridgway <blake@blakeridgway.dev>
2024-05-02 20:49:01 -0500

    (feat): Working on Rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::{Build, Rocket};
+
+#[get("/")]
+fn index() -> &'static str {
+    "Confirming Rocket is working"
+}
+
+#[launch]
+fn rocket() -> Rocket<Build> {
+    rocket::build()
+    .mount("/", routes![index])
 }
